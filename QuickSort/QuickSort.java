008dc9f82b4a7ca958900d073204fb35bd349055
Bintang Villa Devara <11211024@student.itk.ac.id>
2022-10-11 19:13:36 +0800

    Update QuickSort.java

--- QuickSort/QuickSort.java
+++ QuickSort/QuickSort.java
@@ -1,54 +1,53 @@
-public class QuickSort {
-	public static void swap(int[] arr, int i, int j) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
-	
-	public static int partition(int[] arr, int low, int high) {
-		int pivot = arr[high];
+import java.util.*;
+
+class Main {
+	static int partition(int numArray[], int low, int high) {
+		int pivot = numArray[high];
 		int i = (low - 1);
+		System.out.println(Arrays.toString(numArray) + "Pivot = " + pivot);
 		for (int j = low; j <= high - 1; j++) {
-			if (arr[j] < pivot) {
+			if (numArray[j] <= pivot) {
 				i++;
-				swap(arr, i, j);
+				int temp = numArray[i];
+				numArray[i] = numArray[j];
+				numArray[j] = temp;
+				// System.out.println(Arrays.toString(numArray));
 			}
-			printArray(arr, arr.length);
 		}
-
-		System.out.println("Pivot: " + pivot + " = ");
-		printArray(arr, arr.length);
-		// System.out.println(Arrays.toString(arr).replace("[","").replace("]",""));
-		swap(arr, i + 1, high);
-		return (i + 1);
+		System.out.println(Arrays.toString(numArray));
+		int temp = numArray[i + 1];
+		numArray[i + 1] = numArray[high];
+		numArray[high] = temp;
+		return i + 1;
 	}
 
-	public static void quickSort(int[] arr, int low, int high) {
-		if (low < high) {
-			int pivot = partition(arr, low, high);
-			quickSort(arr, low, pivot - 1);
-			quickSort(arr, pivot + 1, high);
-			// printArray(arr, arr.length);
+	static void quickSort(int numArray[], int low, int high) {
+		int[] intStack = new int[high - low + 1];
+		int top = -1;
+		intStack[++top] = low;
+		intStack[++top] = high;
+		while (top >= 0) {
+			high = intStack[top--];
+			low = intStack[top--];
+			int pivot = partition(numArray, low, high);
+
+			if (pivot - 1 > low) {
+				intStack[++top] = low;
+				intStack[++top] = pivot - 1;
+			}
+
+			if (pivot + 1 < high) {
+				intStack[++top] = pivot + 1;
+				intStack[++top] = high;
+			}
 		}
 	}
 
-	public static void printArray(int[] arr, int size) {
-		for (int i = 0; i < size; i++)
-        if (i == arr.length - 1) {
-            System.out.print(arr[i] + "\n");
-        } else {
-            System.out.print(arr[i] + ", ");
-        }
-	}
-
-	public static void main(String[] args) {
-		int[] arr = {7,3,8,6,1,5,2,4};
-		int sort = arr.length;
-		System.out.println("Data sebelum di sorting");
-		printArray(arr, sort);
-		System.out.println("\nProses Quicksort");
-		quickSort(arr, 0, sort - 1);
-		System.out.println("Setelah disorting");
-		printArray(arr, sort);
+	public static void main(String args[]) {
+		int numArray[] = { 3, 2, 6, -1, 9, -6, 1 };
+		int n = 7;
+		System.out.println("Original Array:" + Arrays.toString(numArray));
+		quickSort(numArray, 0, n - 1);
+		System.out.println("\nSorted Array:" + Arrays.toString(numArray));
 	}
 }
