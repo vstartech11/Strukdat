b6fdc586d70ec4bb79324e0355c4a5dedc68d92b
MarksonLDjomie <11211050@student.itk.ac.id>
2022-10-11 20:10:21 +0800

    p

--- 2. Stack, Queue, LinkedList/Main.java
+++ 2. Stack, Queue, LinkedList/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList n = new LinkedList();
+        LinkedList02 n = new LinkedList02();
         n.add(1);
         n.add(4);
         n.add(7);
